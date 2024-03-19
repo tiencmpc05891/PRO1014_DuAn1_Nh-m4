ca20e486fac521fbe045ff87d4e829fa37c9e9ba
MHuy12 <145202827+MHuy12@users.noreply.github.com>
2024-03-19 18:49:05 +0700

    connect database

---
+++ dao/pdo.php
@@ -0,0 +1,79 @@
+<?php
+class Connect
+{
+    // Connection CSDL
+    function pdo_get_connection()
+    {
+        $servername = 'localhost';
+        $dbname = 'wineshop';
+        $username = 'root';
+        $password = 'mysql';
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+
+    function pdo_execute($sql)
+    {
+        $sql_args = array_slice(func_get_args(), 1);
+        try {
+            $conn = $this->pdo_get_connection();
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($sql_args);
+        } catch (PDOException $e) {
+            throw $e;
+        } finally {
+            unset($conn);
+        }
+    }
+
+
+    function pdo_query($sql)
+    {
+        $sql_args = array_slice(func_get_args(), 1);
+        try {
+            $conn = $this->pdo_get_connection();
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($sql_args);
+            $rows = $stmt->fetchAll();
+            return $rows;
+        } catch (PDOException $e) {
+            throw $e;
+        } finally {
+            unset($conn);
+        }
+    }
+
+    // Insert category
+    function pdo_query_one($sql)
+    {
+        $sql_args = array_slice(func_get_args(), 1);
+        try {
+            $conn = $this->pdo_get_connection();
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($sql_args);
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $row;
+        } catch (PDOException $e) {
+            throw $e;
+        } finally {
+            unset($conn);
+        }
+    }
+
+    function pdo_query_value($sql)
+    {
+        $sql_args = array_slice(func_get_args(), 1);
+        try {
+            $conn = $this->pdo_get_connection();
+            $stmt = $conn->prepare($sql);
+            $stmt->execute($sql_args);
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            return array_values($row)[0];
+        } catch (PDOException $e) {
+            $e;
+        } finally {
+            unset($conn);
+        }
+    }
+} 
