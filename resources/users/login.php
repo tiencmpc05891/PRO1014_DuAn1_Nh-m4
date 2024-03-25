479aa08ccdffdd5717d8744e159ee35b132333d6
MHuy12 <145202827+MHuy12@users.noreply.github.com>
2024-03-26 04:02:35 +0700

    login

--- resources/users/login.php
+++ resources/users/login.php
@@ -1,62 +1,61 @@
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$user = $users->sign_in($email);
+	if ($user && isset($password, $user['password'])) { 
+		unset($user['password']); 
+		$_SESSION['user'] = $user; 
+		header("Location: index.php");
+		exit(); 
+	} else {
+		$error = "Email hoặc mật khẩu không hợp lệ";
+	}
+}
+?>
 
-  
-  <!-- ================ start banner area ================= -->	
-	<section class="blog-banner-area" id="category">
-		<div class="container h-100">
-			<div class="blog-banner">
-				<div class="text-center">
-					<h1>Login / Register</h1>
-					<nav aria-label="breadcrumb" class="banner-breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Login/Register</li>
-            </ol>
-          </nav>
+<!--================Login Box Area =================-->
+<section class="login_box_area section-margin mt-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="login_box_img">
+					<div class="hover">
+						<h4>New to our website?</h4>
+						<p>There are advances being made in science and technology everyday, and a good example of this
+							is the</p>
+						<a class="button button-account" href="register.html">Create an Account</a>
+					</div>
 				</div>
 			</div>
-    </div>
-	</section>
-	<!-- ================ end banner area ================= -->
-  
-  <!--================Login Box Area =================-->
-	<section class="login_box_area section-margin">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="login_box_img">
-						<div class="hover">
-							<h4>New to our website?</h4>
-							<p>There are advances being made in science and technology everyday, and a good example of this is the</p>
-							<a class="button button-account" href="register.html">Create an Account</a>
+			<div class="col-lg-6">
+				<div class="login_form_inner">
+					<h3>Log in to enter</h3>
+					<form class="row login_form" action="index.php?url=login" id="contactForm" method="post">
+						<div class="col-md-12 form-group">
+							<input type="text" class="form-control" id="email" name="email" placeholder="Email"
+								onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" required>
 						</div>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<div class="login_form_inner">
-						<h3>Log in to enter</h3>
-						<form class="row login_form" action="#/" id="contactForm" >
+						<div class="col-md-12 form-group">
+							<input type="password" class="form-control" id="password" name="password"
+								placeholder="Password" onfocus="this.placeholder = ''"
+								onblur="this.placeholder = 'Password'" required>
+						</div>
+						<div class="col-md-12 form-group">
+							<button type="submit" value="submit" class="button button-login w-100">Log In</button>
+						</div>
+						<?php if (isset ($error)): ?>
 							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+								<p class="text-danger">
+									<?php echo $error; ?>
+								</p>
 							</div>
-							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
-							</div>
-							<div class="col-md-12 form-group">
-								<div class="creat_account">
-									<input type="checkbox" id="f-option2" name="selector">
-									<label for="f-option2">Keep me logged in</label>
-								</div>
-							</div>
-							<div class="col-md-12 form-group">
-								<button type="submit" value="submit" class="button button-login w-100">Log In</button>
-								<a href="#">Forgot Password?</a>
-							</div>
-						</form>
-					</div>
+						<?php endif; ?>
+						<a href="index.php?url=forgotpassword">quên mật khẩu</a>
+					</form>
 				</div>
 			</div>
 		</div>
-	</section>
-	<!--================End Login Box Area =================-->
-
-
+	</div>
+</section>
+<!--================End Login Box Area =================-->
