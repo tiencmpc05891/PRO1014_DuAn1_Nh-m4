2ad11788791c7cd1155d5279c9735d01f988ec8d
zan0109 <149867083+zan0109@users.noreply.github.com>
2024-03-31 16:18:16 +0700

    dao/blog

--- dao/Blog.php
+++ dao/Blog.php
@@ -12,49 +12,38 @@ class Blog
     {
         $this->database = new Connect();
     }
-    public function insert_baiviet($title, $content, $author, $publish_date, $created_at)
+    public function insert_blog($title, $content, $author)
     {
-        $sql = "INSERT INTO Blog (title, content, author, publish_date, created_at) VALUES (?, ?, ?, ?, ?)";
-        $params = array($title, $content, $author, $publish_date, $created_at);
+        $sql = "INSERT INTO news (title, content, author) VALUES (?, ?, ?)";
+        $params = array($title, $content, $author);
         $this->database->pdo_execute($sql, $params);
     }
-    public function title_td($title)
+   
+   public function get_all_blog()
     {
-        $sql = "SELECT title FROM Blog WHERE title = ?";
-        $params = array($title);
-        $result = $this->database->pdo_query_one($sql, $params);
-        return $result;
-    }    
-    public function content($title)
-    {
-        $sql = "SELECT Content FROM Blog WHERE title = ?";
-        $params = array($title);
-        $result = $this->database->pdo_query_one($sql, $params);
+        $sql = "SELECT * FROM news";
+        $result = $this->database->pdo_query($sql);
         return $result;
     }
-    public function author_tg($title)
+    public function delete_blog($news_id)
     {
-        $sql = "SELECT author FROM Blog WHERE title = ?";
-        $params = array($title);
-        $result = $this->database->pdo_query_one($sql, $params);
+        $sql = "DELETE FROM news  WHERE news_id = ?";
+        $params = array($news_id);
+        $this->database->pdo_execute($sql, $params);
+    }
+    public function update_blog( $title, $content, $author)
+    {  
+        $sql = "UPDATE news SET title = ?, content = ?, author = ? WHERE news_id = ?";
+        $params = array( $title, $content, $author);
+    $this->database->pdo_execute($sql, $params);
+    }
+   
+    public function getone_blog($news_id)
+    {
+        $sql = "SELECT * FROM news WHERE news_id = ?";
+        $params = array($news_id);
+        $result = $this->database->pdo_query($sql, $params);
         return $result;
     }
-    
-    public function publish_date($title)
-    {
-        $sql = "SELECT publish_date FROM Blog WHERE title = ?";
-        $params = array($title);
-        $result = $this->database->pdo_query_one($sql, $params);
-        return $result;
-    }
-    
-    public function created_at($title)
-    {
-        $sql = "SELECT created_at FROM Blog WHERE title = ?";
-        $params = array($title);
-        $result = $this->database->pdo_query_one($sql, $params);
-        return $result;
-    }
-    
 
 }
